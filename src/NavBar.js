1446f3c1b6f1bbd5b71bc8e71fc4b1259eece8c3
David Thornton <david.m.thornton@gmail.com>
2025-05-03 11:08:22 +1000

    Fix error

--- src/NavBar.js
+++ src/NavBar.js
@@ -56,7 +56,6 @@ function NavBar({
         return (
           <View style={[styles.left, leftStyle]}>
             <TouchableOpacity onPress={() => onLeftPress && onLeftPress()} hitSlop={leftHitSlop}>
-              Menu
               <Icon
                 family={leftIconFamily || 'evilicons'}
                 color={leftIconColor || theme.COLORS.ICON}
