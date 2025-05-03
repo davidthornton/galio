8c163e8813502b3caef7f90fe2af860044658107
David Thornton <david.m.thornton@gmail.com>
2025-05-03 11:04:32 +1000

    Further testing

--- src/NavBar.js
+++ src/NavBar.js
@@ -50,7 +50,9 @@ function NavBar({
   function renderLeft() {
     console.log('test');
     if (!hideLeft) {
+      console.log('in not hide left');
       if (leftIconName || (back && !left)) {
+        console.log('in view');
         return (
           <View style={[styles.left, leftStyle]}>
             <TouchableOpacity onPress={() => onLeftPress && onLeftPress()} hitSlop={leftHitSlop}>
